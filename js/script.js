f8550af16937f14b553092b99f26f25bddf0a1c9
elnurruzmanov <elnurruzmanov219autp@gmail.com>
2022-01-08 18:19:58 +0300

    j

--- js/script.js
+++ js/script.js
@@ -1,1 +1,101 @@
 "use strict";
+
+//Selectors
+const musicContainer = document.querySelector("#music-container");
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+const audio = document.querySelector("#music");
+const progress = document.querySelector("#progress");
+const progressContainer = document.querySelector("#progress-container");
+
+const title = document.querySelector("#title");
+const cover = document.querySelector("#cover");
+const images = document.querySelector("#images");
+
+//Song titles;
+
+const songs = ["Aisha Abdul Basith", "Mohammed Tarek", "Sami Yusuf"];
+
+let songIndex = 0;
+
+// Song Details;
+
+loadSong(songs[songIndex]);
+
+//Update Song Details
+
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `musics/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+  images.src = `images/${song}.jpg`;
+  console.log(audio);
+}
+
+// Event Listeners
+
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// //Play Song function
+
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  audio.pause();
+}
+
+//Change song
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+audio.addEventListener("timeupdate", updateProgress);
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+progressContainer.addEventListener("click", setProgress);
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
